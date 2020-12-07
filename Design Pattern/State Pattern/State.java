d8f348f796490892bdc1ac8c3ca1f4bc47344371
Dae Hoon <32921115+daehoon12@users.noreply.github.com>
2020-12-07 12:55:21 +0900

    Update State.java

--- Design Pattern/State Pattern/State.java
+++ Design Pattern/State Pattern/State.java
@@ -1,7 +1,7 @@
 public interface State {
-    void processNumber(String ch);
-    void processOperator(char ch);
-    void printOutResult();
+    void processNumber(String ch); // 정수 처리
+    void processOperator(char ch); // 사칙연산 처리
+    void printOutResult(); // 결과 처리
     String getInput(String s);
     boolean run();
 }
