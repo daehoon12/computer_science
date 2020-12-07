3371587423521880c94c6419858d909103eaf0d5
Dae Hoon <32921115+daehoon12@users.noreply.github.com>
2020-12-07 12:57:14 +0900

    Update Start.java

--- Design Pattern/State Pattern/Start.java
+++ Design Pattern/State Pattern/Start.java
@@ -7,10 +7,10 @@ public class Start implements State {
         this.calc=calc;
     }
     @Override
-    public void processNumber(String ch) {
+    public void processNumber(String ch) { // 정수를 Ooperand1에 set 해주고 FirstOperent State로 바꿈
         int operand1 = Integer.parseInt("" + ch);
         calc.setOperand1(operand1);
-        calc.setState(calc.getFirstOperend());
+        calc.setState(calc.getFirstOperend()); // Translate to FirstOperent
     }
 
     @Override
